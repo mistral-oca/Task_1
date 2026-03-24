124203acdbb34bd2a54563c3d225fb76d3df4037
mrsMistral <obalzak@list.ru>
2026-03-24 22:23:00 +0100

    Разделила тесты name и price

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -18,10 +18,23 @@ class TestBun:
             ("", 10.0),
         ]
     )
-    def test_bun_initialization(self, name, price):
+    def test_bun_name_initialization(self, name, price):
         bun = Bun(name, price)
 
         assert bun.name == name
+
+    @pytest.mark.parametrize(
+        "name, price",
+        [
+            ("black bun", 100.0),
+            ("white bun", 50.5),
+            ("sesame bun", 0),
+            ("", 10.0),
+        ]
+    )
+    def test_bun_price_initialization(self, name, price):
+        bun = Bun(name, price)
+
         assert bun.price == price
 
     @pytest.mark.parametrize(
