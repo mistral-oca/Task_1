e38834ed728e62587cdb3ffb08d10aaaaccc3f74
mrsMistral <obalzak@list.ru>
2026-03-24 22:36:02 +0100

    Разделила тесты в файле test_burger.py

--- tests/test_burger.py
+++ tests/test_burger.py
@@ -10,10 +10,10 @@ from burger import Burger
 class TestBurger:
 
     @pytest.mark.parametrize("bun_price, ingredient_prices, expected_price", [
-        (100, [], 200),                    
-        (100, [50], 250),                  
-        (100, [50, 25], 275),              
-        (0, [10, 20], 30),                 
+        (100, [], 200),
+        (100, [50], 250),
+        (100, [50, 25], 275),
+        (0, [10, 20], 30),
     ])
     def test_get_price(self, bun_price, ingredient_prices, expected_price):
         burger = Burger()
@@ -33,7 +33,7 @@ class TestBurger:
     @pytest.mark.parametrize("index, new_index, expected_order", [
         (0, 2, [2, 3, 1]),
         (2, 0, [3, 1, 2]),
-        (1, 1, [1, 2, 3]),  
+        (1, 1, [1, 2, 3]),
     ])
     def test_move_ingredient(self, index, new_index, expected_order):
         burger = Burger()
@@ -56,7 +56,7 @@ class TestBurger:
         ("SAUCE", "sauce"),
         ("FILLING", "filling"),
     ])
-    def test_get_receipt(self, ingredient_type, expected_type):
+    def test_get_receipt_contains_correct_ingredient_type(self, ingredient_type, expected_type):
         burger = Burger()
 
         bun = Mock()
@@ -72,11 +72,36 @@ class TestBurger:
 
         receipt = burger.get_receipt()
 
-        expected_receipt = (
-            "(==== black bun ====)\n"
-            f"= {expected_type} ketchup =\n"
-            "(==== black bun ====)\n\n"
-            "Price: 250"
-        )
+        assert f"= {expected_type} ketchup =" in receipt
 
-        assert receipt == expected_receipt
+
+    def test_get_receipt_contains_bun(self):
+        burger = Burger()
+
+        bun = Mock()
+        bun.get_name.return_value = "black bun"
+        bun.get_price.return_value = 100
+        burger.set_buns(bun)
+
+        receipt = burger.get_receipt()
+
+        assert "(==== black bun ====)" in receipt
+
+
+    def test_get_receipt_contains_correct_price(self):
+        burger = Burger()
+
+        bun = Mock()
+        bun.get_name.return_value = "black bun"
+        bun.get_price.return_value = 100
+        burger.set_buns(bun)
+
+        ingredient = Mock()
+        ingredient.get_type.return_value = "SAUCE"
+        ingredient.get_name.return_value = "ketchup"
+        ingredient.get_price.return_value = 50
+        burger.add_ingredient(ingredient)
+
+        receipt = burger.get_receipt()
+
+        assert "Price: 250" in receipt
