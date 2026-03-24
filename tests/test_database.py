656392d62ba992de1beb31bc19c1abbb997e9fea
mrsMistral <obalzak@list.ru>
2026-03-24 23:06:08 +0100

    исправила test_database.py

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,84 +1,67 @@
 import pytest
-
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from database import Database
-from bun import Bun
-from ingredient import Ingredient
-from ingredient_types import INGREDIENT_TYPE_SAUCE, INGREDIENT_TYPE_FILLING
+from burger import Burger
 
 
 class TestDatabase:
 
-    def test_available_buns_returns_list(self):
+    def test_burger_price_with_single_bun(self):
         db = Database()
-        buns = db.available_buns()
+        burger = Burger()
 
-        assert isinstance(buns, list)
+        bun = db.available_buns()[0]
+        burger.set_buns(bun)
+        
+        expected_price = bun.get_price() * 2
+        assert burger.get_price() == expected_price
 
-
-    def test_available_buns_contains_bun_objects(self):
+    def test_burger_price_with_bun_and_ingredients(self):
         db = Database()
-        buns = db.available_buns()
+        burger = Burger()
 
-        assert all(isinstance(bun, Bun) for bun in buns)
+        bun = db.available_buns()[0]
+        burger.set_buns(bun)
 
+        ingredient = db.available_ingredients()[0]
+        burger.add_ingredient(ingredient)
 
-    def test_available_buns_length(self):
+        
+        expected_price = bun.get_price() * 2 + ingredient.get_price()
+        assert burger.get_price() == expected_price
+
+    def test_burger_receipt_contains_bun_and_ingredient(self):
         db = Database()
-        buns = db.available_buns()
+        burger = Burger()
 
-        assert len(buns) == 3
+        bun = db.available_buns()[0]
+        burger.set_buns(bun)
 
+        ingredient = db.available_ingredients()[0]
+        burger.add_ingredient(ingredient)
 
-    def test_available_buns_data(self):
+        receipt = burger.get_receipt()
+
+        
+        assert bun.get_name() in receipt
+        assert ingredient.get_name() in receipt
+
+        
+        expected_price = bun.get_price() * 2 + ingredient.get_price()
+        assert f"Price: {expected_price}" in receipt
+
+    def test_burger_with_multiple_ingredients(self):
         db = Database()
-        buns = db.available_buns()
+        burger = Burger()
 
-        names_prices = [(bun.get_name(), bun.get_price()) for bun in buns]
-        expected = [("black bun", 100), ("white bun", 200), ("red bun", 300)]
+        bun = db.available_buns()[0]
+        burger.set_buns(bun)
 
-        assert names_prices == expected
+        ingredients = db.available_ingredients()[:3]
+        for ing in ingredients:
+            burger.add_ingredient(ing)
 
-
-    def test_available_ingredients_returns_list(self):
-        db = Database()
-        ingredients = db.available_ingredients()
-
-        assert isinstance(ingredients, list)
-
-
-    def test_available_ingredients_contains_ingredient_objects(self):
-        db = Database()
-        ingredients = db.available_ingredients()
-
-        assert all(isinstance(ing, Ingredient) for ing in ingredients)
-
-
-    def test_available_ingredients_length(self):
-        db = Database()
-        ingredients = db.available_ingredients()
-
-        assert len(ingredients) == 6
-
-
-    def test_available_ingredients_data(self):
-        db = Database()
-        ingredients = db.available_ingredients()
-
-        types_names_prices = [
-            (ing.get_type(), ing.get_name(), ing.get_price())
-            for ing in ingredients
-        ]
-
-        expected = [
-            (INGREDIENT_TYPE_SAUCE, "hot sauce", 100),
-            (INGREDIENT_TYPE_SAUCE, "sour cream", 200),
-            (INGREDIENT_TYPE_SAUCE, "chili sauce", 300),
-            (INGREDIENT_TYPE_FILLING, "cutlet", 100),
-            (INGREDIENT_TYPE_FILLING, "dinosaur", 200),
-            (INGREDIENT_TYPE_FILLING, "sausage", 300),
-        ]
-
-        assert types_names_prices == expected
+        expected_price = bun.get_price() * 2 + sum(ing.get_price() for ing in ingredients)
+        assert burger.get_price() == expected_price
